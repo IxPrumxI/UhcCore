e663235bea9c67c52eb0afd8b7a50adac994ca3c
Odin Dahlström <zerodind@gmail.com>
2022-09-10 13:53:05 +0200

    Rename setting to "use-default-world-spawn-for-lobby"

--- src/main/java/com/gmail/val59000mc/maploader/MapLoader.java
+++ src/main/java/com/gmail/val59000mc/maploader/MapLoader.java
@@ -326,7 +326,7 @@ public class MapLoader {
 
 		if (config.get(MainConfig.LOBBY_IN_DEFAULT_WORLD)){
 			final World defaultWorld = Bukkit.getWorlds().get(0);
-			final Location spawnLocation = config.get(MainConfig.USE_DEFAULT_WORLD_SPAWN)
+			final Location spawnLocation = config.get(MainConfig.USE_DEFAULT_WORLD_SPAWN_FOR_LOBBY)
 				? defaultWorld.getSpawnLocation().clone().add(0.5, 0, 0.5)
 				: new Location(defaultWorld, 0.5, 100, 0.5);
 			lobby = new Lobby(spawnLocation);
